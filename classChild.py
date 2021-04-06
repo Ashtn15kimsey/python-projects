ac32f483f367230ccf0c5e1a3ac09c1f9710298b
Ashtn15kimsey <79117840+Ashtn15kimsey@users.noreply.github.com>
2021-04-06 12:14:28 -0700

    Add files via upload

---
+++ classChild.py
@@ -0,0 +1,13 @@
+class user:
+    name= 'no name provided'
+    password = '1234abcd'
+    acount_number= 0
+
+class GPA(users):
+    student_gpa = 4.0
+    student_id = "id"
+
+class classes(user):
+    class1 = 'math'
+    class2 = 'science'
+    
