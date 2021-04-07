8adda4b5609fe56f0b1c6fe68e097dcb9d8c5f88
Ashtn15kimsey <79117840+Ashtn15kimsey@users.noreply.github.com>
2021-04-06 21:06:19 -0700

    Add files via upload

--- tkntir.py
+++ tkntir.py
@@ -1,24 +1,32 @@
 import tkinter
 from tkinter import*
 
-class parentwindow(Frame):
+class ParentWindow(Frame):
     def __init__ (self, master):
         Frame.__init__ (self)
 
         self.master = master
-        self.master.ireversable(width=False, height=False)
-        self.master.geomtry('{}x{}'.format(700, 400)
+        self.master.resizable(width=False, height=False)
+        self.master.geometry('{}x{}'.format(700, 400))
         self.master.title('Learning Tkinter!')
+        self.master.config(bg= '#000')
+
+
+        self.varfFname = StringVar()
+        self.varfLname = StringVar()
+        self.varfFname.set('Ash')
+        self.varfLname.set('kimsey')
+
+        print(self.varFname.get())
+        print(self.varLname.get())
+        
+            
         
 
 
-
-
-
-
-if __name__ "__main__":
+if __name__ == "__main__":
     root = Tk()
-    App = parentwidow(root)
+    App = ParentWindow(root)
     root.mainloop()
                       
                             
