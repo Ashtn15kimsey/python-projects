7464db02f2bda4dbb1086d7e38ed2c4ac5d6bced
Ashtn15kimsey <79117840+Ashtn15kimsey@users.noreply.github.com>
2021-04-10 17:55:32 -0700

    Add files via upload

---
+++ webpage_GUI.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import tkinter as tk
+
+
+class ParentWindow(Frame):
+    def __init__ (self, master, *args, **kwargs):
+        Frame.__init__(self, master, *args, **kwargs)
+
+        self.master = master
+        self.master.minsize(400,300) #(Height, Width)
+        self.master.maxsize(300,200)
+        
+        self.btn_add = tk.Button(self.master,width=30,height=2,text='Set a new body text of your choice',command=lambda: web_page_generator.userInput(self))
+        self.btn_add.grid(row=0,column=2,padx=(100,80),pady=(10,0),sticky=W)
+
+        self.txt_body = tk.Entry(self.master,text='',textvariable=lambda: web_page_generator.userInput(self))
+        self.txt_body.grid(row=1,column=2,rowspan=1,columnspan=2,padx=(100,80),pady=(30,40),sticky=N+E+W)
+
+
+                   
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
