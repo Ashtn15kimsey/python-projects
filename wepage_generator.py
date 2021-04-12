53541300b8576b98b0286d5408fa34bdcc5b46cc
Ashtn15kimsey <79117840+Ashtn15kimsey@users.noreply.github.com>
2021-04-12 16:15:05 -0700

    Add files via upload

--- wepage_generator.py
+++ wepage_generator.py
@@ -4,22 +4,25 @@ import webbrowser
 
 
 
-def UserInput(self):
+def userInput(self):
     userInput =  self.txt_body.get()
 
-    # Writing to file 
-    with open("webPage_generator.html", "a") as file:
-        f.write("""<html> 
+   
+    with open("webPage_generator.html", "w") as file1:
+        file1.write("""<html> 
                  <body> 
                     <h1>
-                          Stay tuned for our amazing summer sale! 
-                    </h1>
+                    """
+                       + userInput +   
+                    """</h1>
                  </body> 
             </html>""")
-        f.write(userInput)
+    
     # Reading from file
-    with open("webPage_generator.html", "r+") as f:
-        print(f.read())
+    with open("webPage_generator.html", "r+") as file1:
+        print(file1.read())
+
+    webbrowser.open_new_tab("webPage_generator.html")
 
 
 if __name__ == "__main__":
